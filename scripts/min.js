5ae41afbaa2d44a5c4494de643c2eb6a6dca0100
LoeiFy <LoeiFy@gmail.com>
2018-01-28 19:44:54 +0800

    style fixed

--- scripts/min.js
+++ scripts/min.js
@@ -3,8 +3,8 @@ const pathFn = require('path')
 
 acyort.extend.register('after_build', function (data) {
   if (!acyort.server.running) {
-    const { public_dir } = acyort.config
-    const path = pathFn.join(public_dir, 'css/style.css')
+    const { public_dir, base } = acyort.config
+    const path = pathFn.join(base, public_dir, 'css/style.css')
     const css = acyort.fs.readFileSync(path, 'utf8')
     const min = cssmin(css)
 
