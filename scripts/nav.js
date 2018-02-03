e793103370ba8b3ce5035050cf277e48e8ebd959
LoeiFy <LoeiFy@gmail.com>
2018-02-03 13:11:41 +0800

    paginator

--- scripts/nav.js
+++ scripts/nav.js
@@ -2,6 +2,7 @@ const {
   docs_nav: docsNav,
   api_nav: apiNav,
 } = acyort.config
+const { __ } = acyort.helper.methods
 
 function nav(data, prefix) {
   const html = ['<ul>', '', '</ul>']
@@ -21,10 +22,49 @@ function nav(data, prefix) {
   return html.join('')
 }
 
+function getLinks(nav) {
+  return Object.values(nav)
+    .reduce((prev, next) => prev.concat(next), [])
+    .map(link => link.toLowerCase())
+}
+
+const allLinks = {
+  docs: getLinks(docsNav),
+  api: getLinks(apiNav)
+}
+
 acyort.extend.helper('_docsNav', function () {
   return nav(docsNav, 'docs')
 })
 
 acyort.extend.helper('_apiNav', function () {
   return nav(apiNav, 'api')
+})
+
+acyort.extend.helper('_paginator', function ({ name, category }) {
+  if (!category) {
+    return ''
+  }
+
+  const prefix = category === 'API' ? 'api' : 'docs'
+  const links = category === 'API' ? allLinks.api : allLinks.docs
+  const index = links.indexOf(name.toLowerCase())
+
+  function linkTag(i, dire) {
+    const item = dire === 'prev' ? links[i - 1] : links[i + 1]
+    if (item === 'overview') {
+      return `<a class="${dire}" href="/${prefix}/">${__(`paginator.${dire}`)}</a>`
+    }
+    return `<a class="${dire}" href="/${prefix}/${item}/">${__(`paginator.${dire}`)}</a>`
+  }
+
+  if (index === -1) {
+    return linkTag(index + 1, 'next')
+  }
+
+  if (index === links.length - 1) {
+    return linkTag(index, 'prev')
+  }
+
+  return linkTag(index, 'prev') + linkTag(index, 'next')
 })
