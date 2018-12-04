b60e37d60fa508031a35e68aedc51039f359348b
LoeiFy <LoeiFy@gmail.com>
2018-12-04 23:03:48 +0800

    feat: process pages data

--- scripts/processor.js
+++ scripts/processor.js
@@ -1,16 +1,31 @@
 module.exports = function processor() {
+  const regex = /^\[(.+?)]/
   const { authors } = this.config
   const issues = this.store.get('issues')
-    .filter(({ user }) => authors.includes(user.login))
-    .map(issue => ({
-      id: issue.id,
-      title: issue.title,
-      tags: issue.labels.map(({ name }) => name),
-      category: (issue.milestone || {}).title,
-      updated: issue.updated_at,
-      content: issue.body,
-    }))
+  const pages = issues
+    .filter(({ user, title }) => authors.includes(user.login) && regex.test(title))
+    .map((issue) => {
+      const {
+        id,
+        title,
+        labels,
+        milestone,
+        body,
+      } = issue
+      const matched = title.split(regex)
 
+      return {
+        id,
+        title: matched[2],
+        url: `/${matched[1]}/`,
+        path: `/${matched[1]}/index.html`,
+        language: labels.map(({ name }) => name)[0] || 'en',
+        category: (milestone || {}).title,
+        content: this.renderer.render('markdown', body, { lineNumbers: false }),
+      }
+    })
+
+  this.store.set('pages', pages)
   this.store.set('updatedAt', issues[0].updated)
   this.store.set('version', this.version)
 }
