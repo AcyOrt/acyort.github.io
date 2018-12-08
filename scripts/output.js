e492f15967f0fe5d1281238fdfc7e1cc68792b75
LoeiFy <LoeiFy@gmail.com>
2018-12-08 14:29:54 +0800

    fix: switch language

--- scripts/output.js
+++ scripts/output.js
@@ -1,10 +1,12 @@
 module.exports = function output() {
-  this.store.get('pages').forEach((page) => {
-    const { path } = page
-    this.outputHTML({
-      template: 'doc',
-      path,
-      data: page,
+  this.store.get('pages')
+    .forEach((page) => {
+      const { path, language } = page
+      this.helper.language = language
+      this.outputHTML({
+        template: 'doc',
+        path,
+        data: page,
+      })
     })
-  })
 }
